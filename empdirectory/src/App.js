73e74b4b5b7c04ae52144670d058a4ba9cbca50c
Evan Iswahyudi <evan_iswahyu@yahoo.com>
2020-02-09 23:32:05 -0800

    updated2

--- empdirectory/src/App.js
+++ empdirectory/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
+
 // import Footer from "./components/Footer";
 // import Wrapper from "./components/Wrapper";
 
--- empdirectory/src/pages/Search.js
+++ empdirectory/src/pages/Search.js
@@ -1,33 +1,61 @@
-import React from "react";
-import "./style.css";
+// import React, { Component } from "react";
+// import API from "../utils/API";
+// import Container from "../components/Container";
+// import SearchForm from "../components/SearchForm";
+// import SearchResults from "../components/SearchResults";
+// import Alert from "../components/Alert";
 
-// Using the datalist element we can create autofill suggestions based on the props.breeds array
-function SearchForm(props) {
-  return (
-    <form className="search">
-      <div className="form-group">
-        <label htmlFor="breed">Breed Name:</label>
-        <input
-          value={props.search}
-          onChange={props.handleInputChange}
-          name="breed"
-          list="breeds"
-          type="text"
-          className="form-control"
-          placeholder="Type in a dog breed to begin"
-          id="breed"
-        />
-        <datalist id="breeds">
-          {props.breeds.map(breed => (
-            <option value={breed} key={breed} />
-          ))}
-        </datalist>
-        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
-          Search
-        </button>
-      </div>
-    </form>
-  );
-}
+// class Search extends Component {
+//   state = {
+//     search: "",
+//     breeds: [],
+//     results: [],
+//     error: ""
+//   };
 
-export default SearchForm;
+//   // When the component mounts, get a list of all available base breeds and update this.state.breeds
+//   componentDidMount() {
+//     API.getBaseBreedsList()
+//       .then(res => this.setState({ breeds: res.data.message }))
+//       .catch(err => console.log(err));
+//   }
+
+//   handleInputChange = event => {
+//     this.setState({ search: event.target.value });
+//   };
+
+//   handleFormSubmit = event => {
+//     event.preventDefault();
+//     API.getDogsOfBreed(this.state.search)
+//       .then(res => {
+//         if (res.data.status === "error") {
+//           throw new Error(res.data.message);
+//         }
+//         this.setState({ results: res.data.message, error: "" });
+//       })
+//       .catch(err => this.setState({ error: err.message }));
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <Container style={{ minHeight: "80%" }}>
+//           <h1 className="text-center">Search By Breed!</h1>
+//           <Alert
+//             type="danger"
+//             style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
+//           >
+//             {this.state.error}
+//           </Alert>
+//           <SearchForm
+//             handleFormSubmit={this.handleFormSubmit}
+//             handleInputChange={this.handleInputChange}
+//             breeds={this.state.breeds}
+//           />
+//           <SearchResults results={this.state.results} />
+//         </Container>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Search;
